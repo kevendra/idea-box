ae50a1d44e73bea1b508ca748c9c143522123c26
kuldeepJadhav <kuldeep.jadhav@live.com>
2015-02-12 15:12:56 +0530

    Start new server on 3001

--- idea-box-server/app.js
+++ idea-box-server/app.js
@@ -12,6 +12,7 @@ var bodyParserExpress = require("body-parser");
 var sessionExpress = require("cookie-session");
 var loggerExpress = require("morgan");
 var mongoose = require('mongoose');
+var staticExpress = require("serve-static");
 
 
 
@@ -19,20 +20,21 @@ var app = express();
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
-app.use('/',express.static(__dirname + '/public'));
-app.use('/',express.static(__dirname + '/'));
+// app.use('/',express.static(__dirname + '/public'));
+// app.use('/',express.static(__dirname + '/'));
 
-app.use('/public',express.static(__dirname + '/public'));
-app.use('/bower_components',express.static(__dirname + '/bower_components'));
+// app.use('/public',express.static(__dirname + '/public'));
+// app.use('/bower_components',express.static(__dirname + '/bower_components'));
 
 //app.set('views', __dirname + '/views');
 
+ app.use(staticExpress(path.join(__dirname, 'public')));
 
 
 
 
 
-app.set('port', process.env.PORT || 8090);
+app.set('port',3001);
 app.use(cookieParser());
 app.use(sessionExpress({
     secret: '1234567890'
@@ -54,7 +56,7 @@ app.use('/api/admin/idea', basicAuth(username, password));
 
 //mongoose.connect('mongodb://localhost:27017/ideabox2');
 //var url = 'mongodb://localhost:27017/ideabox';
-mongoose.connect('mongodb://localhost:27017/ideaboxtest');
+mongoose.connect('mongodb://localhost:27017/ideabox');
 mongoose.model('Idea', require('./models/idea').Idea);
 
 var idea = require('./config/config').idea;
