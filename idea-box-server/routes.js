e93ba5fcdcd0190b86e65aea8eb25dfd3c0673e5
kuldeepJadhav <kuldeep.jadhav@live.com>
2015-03-06 10:27:11 +0530

    Moved all the routes to another file

---
+++ idea-box-server/routes.js
@@ -0,0 +1,11 @@
+
+var idea = require('./config/config').idea;
+var user = require('./config/config').user;
+
+function routes(app) {
+    app.get('/api/idea/idea-list.json', idea.list);
+    app.post('/api/idea/add-update-idea.json', idea.addOrUpdateIdea);
+    app.post('/api/user/add-update-user.json', user.saveOrUpdateThirdPartyUser);
+    app.get('/api/user/get-logged-in-user.json', user.getLoggedInUser);
+}
+module.exports = routes;
