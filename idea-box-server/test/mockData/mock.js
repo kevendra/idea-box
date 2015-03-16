c550d2aebdea9bd96ea67c024f9044c04a48b8e7
kuldeepJadhav <kuldeep.jadhav@live.com>
2015-03-16 10:08:05 +0530

    Adding mock data for tests

---
+++ idea-box-server/test/mockData/mock.js
@@ -0,0 +1,17 @@
+var mockIdeas = [{
+        'name': 'Idea1'
+    }, {
+        'name': 'Idea2'
+    }, {
+        'name': 'Idea3'
+    }, {
+        'name': 'Idea4'
+    }
+
+];
+
+var mock = {
+	'mockIdeas': mockIdeas
+}
+
+module.exports = mock
