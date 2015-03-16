5b5e97a9cd3a0146b6b17547f242ab46bdcb451a
kuldeepJadhav <kuldeep.jadhav@live.com>
2015-03-16 10:06:53 +0530

    Added a test

--- idea-box-server/test/listSpec.js
+++ idea-box-server/test/listSpec.js
@@ -8,7 +8,7 @@
  var dependency = require('../api/dependency');
  var treeStructure = require('../api/idea/generateTreeStructure');
  var _ = require('lodash');
-
+ var mock = require('./mockData/mock');
 
  var sandbox;
 
@@ -34,13 +34,13 @@
                  'sort': function(sortQuery) {
                      expect(sortQuery).to.deep.equal('-createdOn');
                      return {
-                        'lean': function() {
-                            return {
-                                'exec': function(callback) {
-                                    callback('error');
-                                }
-                            };
-                        }
+                         'lean': function() {
+                             return {
+                                 'exec': function(callback) {
+                                     callback('error');
+                                 }
+                             };
+                         }
                      }
                  }
              };
@@ -50,20 +50,79 @@
              .get('/api/idea/idea-list.json?category=test')
              .expect(500)
              .end(function(err, res) {
-                if(err) {
-                    done(err);
-                }else {
-                    done();
-                }
+                 if (err) {
+                     done(err);
+                 } else {
+                     done();
+                 }
              });
-
-
-
      });
 
 
+     it('should return the idea list....', function(done) {
+         var treeStructure = require('../api/idea/generateTreeStructure');
+         var mockIdeaList
+         var Idea = dependency.getIdeaModel();
+
+         sandbox.stub(Idea, 'find', function(query) {
+             expect(query).to.deep.equal({
+                 category: 'test'
+             });
+
+             return {
+                 'sort': function(sortQuery) {
+                     expect(sortQuery).to.deep.equal('-createdOn');
+                     return {
+                         'lean': function() {
+                             return {
+                                 'exec': function(callback) {
+
+                                     callback(undefined, []);
+                                 }
+                             };
+                         }
+                     }
+                 }
+             };
+         });
+
+         sinon.stub(treeStructure, 'addParentIdIfMissing', function() {});
+
+         sinon.stub(treeStructure, 'unflatten', function() {
+             return mock.mockIdeas;
+         });
 
 
+         request(app)
+             .get('/api/idea/idea-list.json?category=test')
+             .expect(200)
+             .end(function(err, res) {
+                 if (err) {
+                     done(err);
+                 } else {
+                     expect(res.body).to.deep.equal({
+                         "status": "SUCCESS",
+                         "data": {
+                             ideaList: [{
+                                     'name': 'Idea1'
+                                 }, {
+                                     'name': 'Idea2'
+                                 }, {
+                                     'name': 'Idea3'
+                                 }, {
+                                     'name': 'Idea4'
+                                 }
+
+                             ]
+                         },
+                         "error": null
+                     });
+
+                     done();
+                 }
+             });
+
+     });
 
 
  });
